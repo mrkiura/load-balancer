c171aae5c8b45c6295f2639006d4f244b12d8626
Alex Kiura <kiuraalex@gmail.com>
2023-01-09 12:49:16 +0300

    Test host based routing

--- test_loadbalancer.py
+++ test_loadbalancer.py
@@ -9,7 +9,14 @@ def client():
     with loadbalancer.test_client() as client:
         yield client
 
+def test_host_routing_tururu(client):
+    result = client.get('/', headers={'Host': 'tururu.com'})
+    assert b'tururu' in result.data
 
-def test_hello_client(client):
-    response = client.get('/')
-    assert "hello" in response.data.decode('utf-8').lower()
+def test_host_routing_wawawa(client):
+    result = client.get('/',  headers={'Host': 'wawawa.com'})
+
+def test_hello_routing_not_found(client):
+    result = client.get('/', headers={'Host': 'wuuuuui.com'})
+    assert b'Not Found' in result.data
+    assert 404 == result.status_code
