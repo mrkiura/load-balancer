280362b318cbf7a3c5647c1e26d37c394615a8a5
Alex Kiura <kiuraalex@gmail.com>
2023-01-12 11:28:24 +0300

    Implement Host based routing

--- loadbalancer.py
+++ loadbalancer.py
@@ -1,9 +1,26 @@
 from flask import Flask
+import random
+import requests
+from flask import Flask, request
 
 
 loadbalancer = Flask(__name__)
 
+MANGO_BACKENDS = ['localhost:8081', 'localhost:8082']
+APPLE_BACKENDS = ['localhost:9081', 'localhost:9082']
+
 
 @loadbalancer.route('/')
-def index():
-    return 'Hello, World!'
+def router():
+    host_header = request.headers.get('Host')
+    if host_header == 'www.mango.com':
+        response = requests.get(f'http://{random.choice(MANGO_BACKENDS)}')
+        backend = random.choice(MANGO_BACKENDS)
+        return response.content, response.status_code
+    elif host_header == 'www.apple.com':
+        response = requests.get(f'http://{random.choice(APPLE_BACKENDS)}')
+        backend = random.choice(APPLE_BACKENDS)
+        return response.content, response.status_code
+    else:
+        return 'Not Found. tururu', 404
+
