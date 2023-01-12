132b6a0879abad375baf5827a3e56c07a2ea9a85
Alex Kiura <kiuraalex@gmail.com>
2023-01-12 14:56:36 +0300

    Change load balancer hosts:

--- loadbalancer.py
+++ loadbalancer.py
@@ -6,21 +6,34 @@ from flask import Flask, request
 
 loadbalancer = Flask(__name__)
 
-MANGO_BACKENDS = ['localhost:8081', 'localhost:8082']
-APPLE_BACKENDS = ['localhost:9081', 'localhost:9082']
+TURURU_BACKENDS = ['localhost:8081', 'localhost:8082']
+WAWAWA_BACKENDS = ['localhost:9081', 'localhost:9082']
 
 
 @loadbalancer.route('/')
 def router():
     host_header = request.headers.get('Host')
-    if host_header == 'www.mango.com':
-        response = requests.get(f'http://{random.choice(MANGO_BACKENDS)}')
-        backend = random.choice(MANGO_BACKENDS)
+    if host_header == 'www.tururu.com':
+        backend = random.choice(TURURU_BACKENDS)
+        response = requests.get(f'http://{backend}')
         return response.content, response.status_code
-    elif host_header == 'www.apple.com':
-        response = requests.get(f'http://{random.choice(APPLE_BACKENDS)}')
-        backend = random.choice(APPLE_BACKENDS)
+    elif host_header == 'www.wawawa.com':
+        backend = random.choice(WAWAWA_BACKENDS)
+        response = requests.get(f'http://{backend}')
         return response.content, response.status_code
     else:
         return 'Not Found. tururu', 404
 
+
+@loadbalancer.route('/tururu')
+def tururu():
+    backend = random.choice(TURURU_BACKENDS)
+    response = requests.get(f'http://{backend}')
+    return response.content, response.status_code
+
+
+@loadbalancer.route('/wawawa')
+def wawawa():
+    backend = random.choice(WAWAWA_BACKENDS)
+    response = requests.get(f'http://{backend}')
+    return response.content, response.status_code
