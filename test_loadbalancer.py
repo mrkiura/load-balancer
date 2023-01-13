14c79fa3eb5cf33c84b4e5216e7206165e000901
Alex Kiura <kiuraalex@gmail.com>
2023-01-13 23:31:10 +0300

    Test path based routing

--- test_loadbalancer.py
+++ test_loadbalancer.py
@@ -20,3 +20,19 @@ def test_hello_routing_not_found(client):
     result = client.get('/', headers={'Host': 'wuuuuui.com'})
     assert b'Not Found' in result.data
     assert 404 == result.status_code
+
+
+def test_path_routing_tururu(client):
+    result = client.get('/tururu')
+    assert b'This is a tururu application.' in result.data
+
+
+def test_path_routing_wawawa(client):
+    result = client.get('/wawawa')
+    assert b'This is a wawawa application.' in result.data
+
+
+def test_path_routing_not_found(client):
+    result = client.get('/wuuuuui')
+    assert b'Not Found' in result.data
+    assert 404 == result.status_code
