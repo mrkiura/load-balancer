9bc2dc872bc4585e234755ab679f5486d19ce46a
Alex Kiura <kiuraalex@gmail.com>
2023-01-17 14:35:39 +0300

    Create server model

---
+++ models.py
@@ -0,0 +1,28 @@
+import requests
+
+
+class Server:
+    def __init__(self, endpoint: str, path:str="/healthcheck"):
+        self.endpoint = endpoint
+        self.path = path
+        self.healthy = False
+        self.scheme = "http://"
+        self.timeout = 1
+
+    def healthcheck_and_update_status(self):
+        try:
+            response = requests.get(self.scheme + self.endpoint + self.path, timeout=self.timeout)
+            if response.ok:
+                self.healthy = True
+            else:
+                self.healthy = False
+        except (requests.exceptions.ConnectionError, requests.exceptions.Timeout):
+            self.healthy = False
+
+    def __eq__(self, other: object) -> bool:
+        if isinstance(other, Server):
+            return self.endpoint == other.endpoint
+        return False
+
+    def __repr__(self) -> str:
+        return f'<Server: {self.endpoint} {self.healthy} {self.timeout}>'
