77fec96106730f23632250a9e5852e01fd42f0fe
Alex Kiura <kiuraalex@gmail.com>
2023-01-17 14:35:16 +0300

    Add type hints for servers

--- config.py
+++ config.py
@@ -1,7 +1,22 @@
 import yaml
+from typing import TypedDict, List, Sequence
 
 
-def load_config(path):
+
+class HostServers(TypedDict):
+    host: str
+    servers: Sequence[str]
+
+class PathServers(TypedDict):
+    path: str
+    servers: Sequence[str]
+
+class Config(TypedDict):
+    hosts: Sequence[HostServers]
+    paths: Sequence[PathServers]
+
+
+def load_config(path: str) -> Config:
     with open(path) as config_file:
         config = yaml.load(config_file, Loader=yaml.FullLoader)
     return config
