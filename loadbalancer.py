02a8c4bca1a1f3a07544e626350e9e32894d737b
Alex Kiura <kiuraalex@gmail.com>
2023-01-25 09:25:54 +0300

    Only route to healthy servers

--- loadbalancer.py
+++ loadbalancer.py
@@ -1,24 +1,34 @@
 from flask import Flask
 import random
 import requests
-from config import config
 from flask import Flask, request
 
+from utils import (
+    get_healthy_server,
+    healthcheck,
+    load_config,
+    transform_backends_from_config
+)
 
 loadbalancer = Flask(__name__)
 
-TURURU_BACKENDS = ['localhost:8081', 'localhost:8082']
-WAWAWA_BACKENDS = ['localhost:9081', 'localhost:9082']
-
+config = load_config("loadbalancer.yaml")
+register = transform_backends_from_config(config)
 
 
 @loadbalancer.route('/')
 def router():
+    """Route requests to the correct backend server based on the host.
+    """
+    updated_register = healthcheck(register)
+    print("updated_register", updated_register)
     host_header = request.headers.get('Host')
     for entry in config['hosts']:
         if host_header == entry['host']:
-            backend = random.choice(entry["servers"])
-            response = requests.get(f'http://{backend}')
+            healthy_server = get_healthy_server(entry["host"], updated_register)
+            if not healthy_server:
+                return 'No backend servers available.', 503
+            response = requests.get(f'http://{healthy_server.endpoint}')
             return response.content, response.status_code
 
     else:
@@ -27,6 +37,7 @@ def router():
 
 @loadbalancer.route("/<path>")
 def path_router(path):
+    """Route requests to the correct backend server based on the path."""
     for entry in config["paths"]:
         if f"/{path}" == entry["path"]:
             backend = random.choice(entry["servers"])
