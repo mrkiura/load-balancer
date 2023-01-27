47f5d1b63380dfabc47a3eab66d76fe6856bebfd
Alex Kiura <kiuraalex@gmail.com>
2023-01-27 13:14:16 +0300

    Test for the correct host

--- test_loadbalancer.py
+++ test_loadbalancer.py
@@ -9,15 +9,24 @@ def client():
     with loadbalancer.test_client() as client:
         yield client
 
+
 def test_host_routing_tururu(client):
-    result = client.get('/', headers={'Host': 'tururu.com'})
-    assert b'tururu' in result.data
+    result = client.get('/', headers={'Host': 'www.tururu.com'})
+    assert b'This is a tururu application' in result.data
+
 
 def test_host_routing_wawawa(client):
-    result = client.get('/',  headers={'Host': 'wawawa.com'})
+    result = client.get('/',  headers={'Host': 'www.wawawa.com'})
+    assert b'This is a wawawa application' in result.data
 
-def test_hello_routing_not_found(client):
-    result = client.get('/', headers={'Host': 'wuuuuui.com'})
+
+def test_host_routing_orange(client):
+    result = client.get('/', headers={'Host': 'www.orange.com'})
+    assert b'No backend servers available.' in result.data
+
+
+def test_host_routing_not_found(client):
+    result = client.get('/', headers={'Host': 'www.wuuuuui.com'})
     assert b'Not Found' in result.data
     assert 404 == result.status_code
 
