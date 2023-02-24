fcc182c659c2cfea32069561e1567bd6736154e6
Alex Kiura <kiuraalex@gmail.com>
2023-02-24 10:56:04 +0300

    Add json response with headers

--- src/app.py
+++ src/app.py
@@ -1,13 +1,18 @@
 import os
 
-from flask import Flask
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def sample():
-    return f"This is a {os.environ['APP']} application."
+    return jsonify(
+        message=f'This is the {os.environ["APP"]} application.',
+        server=request.base_url,
+        custom_header=request.headers.get('MyCustomHeader', None),
+        host_header=request.headers.get('Host', request.base_url),
+    )
 
 
 @app.route("/healthcheck")
