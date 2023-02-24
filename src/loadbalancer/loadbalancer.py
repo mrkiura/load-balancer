1e741da3aafa5442811a49169280292b722195f1
Alex Kiura <kiuraalex@gmail.com>
2023-02-24 11:32:13 +0300

    Process header rules for path based routing

--- src/loadbalancer/loadbalancer.py
+++ src/loadbalancer/loadbalancer.py
@@ -43,6 +43,7 @@ def path_router(path):
             healthy_server = get_healthy_server(entry["path"], updated_register)
             if not healthy_server:
                 return 'No backend servers available.', 503
-            response = requests.get(f'http://{healthy_server.endpoint}')
+            headers = process_header_rules(config, host_header, {k: v for k, v in request.headers.items()})
+            response = requests.get(f'http://{healthy_server.endpoint}', headers=headers)
             return response.content, response.status_code
     return 'Not Found.', 404
