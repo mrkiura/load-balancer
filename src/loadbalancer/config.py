8f07d8ecb187f60652d3a1e09a29792abfdaae06
Alex Kiura <kiuraalex@gmail.com>
2023-02-24 11:30:52 +0300

    Use the correct path

--- src/loadbalancer/config.py
+++ src/loadbalancer/config.py
@@ -3,14 +3,15 @@ import os
 from typing import TypedDict, List, Sequence
 
 
-
 class HostServers(TypedDict):
     host: str
     servers: Sequence[str]
 
+
 class PathServers(TypedDict):
     path: str
     servers: Sequence[str]
+
 
 class Config(TypedDict):
     hosts: Sequence[HostServers]
@@ -22,5 +23,6 @@ def load_config(path: str) -> Config:
         config = yaml.load(config_file, Loader=yaml.FullLoader)
     return config
 
-filename = './src/loadbalancer/loadbalancer.yaml'
+
+filename = 'loadbalancer.yaml'
 config = load_config(os.path.abspath(filename))
